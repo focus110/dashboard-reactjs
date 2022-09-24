8266372e6ef22baad895e4435073abcc2aa0e980
focus110 <iamemmaochuko@gmail.com>
2022-09-24 02:04:50 +0100

    page routing & folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import Main from "./Pages/Main/Main";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
